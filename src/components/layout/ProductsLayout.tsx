c5f7eea1b2212f47d6e87960e9859e813f4c6792
miko <dictcalabarzon@gmail.com>
2025-03-05 13:28:52 +0800

    Removed Notif bell

--- src/components/layout/ProductsLayout.tsx
+++ src/components/layout/ProductsLayout.tsx
@@ -36,27 +36,6 @@ export const ProductsLayout = ({ children }: DashboardLayoutProps) => {
         
         {/* Main Content */}
         <div className="flex-1 flex flex-col min-h-screen">
-          <header className="h-16 border-b bg-white/50 backdrop-blur-sm flex items-center justify-between px-6">
-            <h1 className="text-2xl font-semibold text-vendor-800">Pasalubong</h1>
-            <div className="flex items-center space-x-4">
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" size="icon" className="relative">
-                    <Bell className="h-5 w-5" />
-                    <span className="absolute -top-1 -right-1 h-4 w-4 bg-red-500 rounded-full text-[10px] text-white flex items-center justify-center">
-                      3
-                    </span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end" className="w-[300px]">
-                  <DropdownMenuItem>
-                    <ShoppingBag className="mr-2 h-4 w-4" />
-                    <span>New order received</span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </header>
           <main className="flex-1 p-6">{children}</main>
         </div>
       </div>
