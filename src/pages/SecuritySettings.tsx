9f09da1b9ff1ea6af9f7c1518e344ea388a2df9b
Mikko <kmdeluna01@gmail.com>
2025-06-24 17:38:24 +0800

    Added POS Account Configuration

--- src/pages/SecuritySettings.tsx
+++ src/pages/SecuritySettings.tsx
@@ -10,13 +10,19 @@ const baseURL = import.meta.env.VITE_API_URL || "";
 const SecuritySettings = () => {
     // navigate function for routing within the app
     const navigate = useNavigate();
-    
+
     // useState hooks to manage component state
     const [showVerifyModal, setShowVerifyModal] = useState(false); // Controls the visibility of the verify password modal
+    const [showPOSAccountModal, setShowPOSAccountModal] = useState(false); // Controls the visibility of the POS account modal
+    const [newPOSUsername, setPOSUsername] = useState(''); // State for storing the current POS username input
+    const [newPOSPin, setPOSPin] = useState(''); // State for storing the current POS PIN input
     const [showChangePasswordModal, setShowChangePasswordModal] = useState(false); // Controls visibility of change password modal
     const [password, setPassword] = useState(''); // State for storing the current password input
     const [newPassword, setNewPassword] = useState(''); // State for storing the new password input
     const [isLoading, setIsLoading] = useState(false); // State for managing the loading state of the process
+    const [verifyFor, setVerifyFor] = useState(""); // either "password" or "pos"
+    const [posPinError, setPosPinError] = useState('');
+
 
     // Function to clear password input fields
     const clearInputs = () => {
@@ -36,11 +42,17 @@ const SecuritySettings = () => {
             });
 
             if (response.status === 200) {
-                toast({ title: "Success", description: "Password verified!" }); // Show success toast if password is correct
-                setShowVerifyModal(false); // Close the verify password modal
-                setShowChangePasswordModal(true); // Open the change password modal
-                setPassword(''); // Clear the current password input field
+                toast({ title: "Success", description: "Password verified!" });
+                setShowVerifyModal(false);
+                setPassword('');
+
+                if (verifyFor === "password") {
+                    setShowChangePasswordModal(true);
+                } else if (verifyFor === "pos") {
+                    setShowPOSAccountModal(true);
+                }
             }
+
         } catch (error) {
             setPassword(''); // Clear password field on error
             if (error.response && error.response.status === 400) {
@@ -86,16 +98,60 @@ const SecuritySettings = () => {
         }
     };
 
+    const handlePOSAccount = async () => {
+        setIsLoading(true); // Set loading state to true
+        const vendorId = localStorage.getItem('vendorId'); // Get vendorId from local storage
+
+        try {
+            // Send POST request to change the POS account
+            const response = await axios.post(`${baseURL}/merchant-profile/${vendorId}/change-POSAccount`, {
+                newPOSUsername,
+                newPOSPin
+            });
+
+            if (response.status === 200) {
+                toast({ title: "Success", description: "POS Account changed successfully!" }); // Show success toast on success
+                setShowPOSAccountModal(false); // Close the POS account modal
+                navigate("/merchant/security-settings"); // Redirect to the security settings page
+                clearInputs(); // Clear input fields
+            }
+        } catch (error) {
+            setPOSUsername(''); // Clear the POS username field on error
+            setPOSPin(''); // Clear the POS PIN field on error
+            if (error.response && error.response.status === 400) {
+                // Show an error toast if the POS account change failed
+                toast({ title: "Error", description: "Failed to change POS Account. Please try again.", variant: "destructive" });
+            } else {
+                // Show a generic error toast for any other error
+                toast({ title: "Error", description: "Something went wrong. Please try again.", variant: "destructive" });
+            }
+        } finally {
+            setIsLoading(false); // Set loading state to false when done
+        }
+    };
+
 
     return (
         <div className="min-h-screen">
             <div className="flex items-center mb-6">
                 <h2 className="text-2xl font-bold text-gray-800">Security Settings</h2>
             </div>
-            <div className="m-3">
-                <button 
+            <div className="m-3 space-y-4">
+                <button
                     className="flex justify-between items-center w-full bg-white p-4 rounded-lg shadow hover:bg-green-50"
-                    onClick={() => setShowVerifyModal(true)}
+                    onClick={() => {
+                        setVerifyFor("pos");
+                        setShowVerifyModal(true);
+                    }}
+                >
+                    <span className="text-lg font-semibold text-green-700">Change POS Account</span>
+                </button>
+                <button
+                    className="flex justify-between items-center w-full bg-white p-4 rounded-lg shadow hover:bg-green-50"
+                    onClick={() => {
+                        setVerifyFor("password");
+                        setShowVerifyModal(true);
+                    }}
                 >
                     <span className="text-lg font-semibold text-green-700">Change Password</span>
                 </button>
@@ -106,15 +162,15 @@ const SecuritySettings = () => {
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
                     <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
                         <h2 className="text-xl font-bold mb-4">Verify Your Password</h2>
-                        <input 
-                            type="password" 
-                            placeholder="Enter your current password" 
-                            value={password} 
-                            onChange={(e) => setPassword(e.target.value)} 
+                        <input
+                            type="password"
+                            placeholder="Enter your current password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className="w-full p-2 border rounded-lg mb-4"
                         />
                         <div className="flex justify-end gap-2">
-                            <button 
+                            <button
                                 className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
                                 onClick={() => {
                                     setShowVerifyModal(false);
@@ -123,7 +179,7 @@ const SecuritySettings = () => {
                             >
                                 Cancel
                             </button>
-                            <button 
+                            <button
                                 className="px-4 py-2 bg-green-700 text-white rounded-lg hover:bg-green-600"
                                 onClick={verifyPassword}
                                 disabled={isLoading}
@@ -140,15 +196,15 @@ const SecuritySettings = () => {
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
                     <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
                         <h2 className="text-xl font-bold mb-4">Change Your Password</h2>
-                        <input 
-                            type="password" 
-                            placeholder="Enter new password" 
-                            value={newPassword} 
-                            onChange={(e) => setNewPassword(e.target.value)} 
+                        <input
+                            type="password"
+                            placeholder="Enter new password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
                             className="w-full p-2 border rounded-lg mb-4"
                         />
                         <div className="flex justify-end gap-2">
-                            <button 
+                            <button
                                 className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
                                 onClick={() => {
                                     setShowChangePasswordModal(false);
@@ -157,13 +213,76 @@ const SecuritySettings = () => {
                             >
                                 Cancel
                             </button>
-                            <button 
+                            <button
                                 className="px-4 py-2 bg-green-700 text-white rounded-lg hover:bg-green-600"
                                 onClick={handleChangePassword}
                                 disabled={isLoading}
                             >
                                 {isLoading ? "Changing..." : "Change Password"}
                             </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {/* Change Password Modal */}
+            {showPOSAccountModal && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+                        <h2 className="text-xl font-bold mb-4">Change Your POS Account</h2>
+                        <input
+                            type="text"
+                            placeholder="Enter new POS Username"
+                            value={newPOSUsername}
+                            onChange={(e) => setPOSUsername(e.target.value)}
+                            className="w-full p-2 border rounded-lg mb-4"
+                        />
+                        <div className="mb-4">
+                            <input
+                                type="password"
+                                placeholder="Enter new POS PIN"
+                                value={newPOSPin}
+                                onChange={(e) => {
+                                    const value = e.target.value;
+                                    if (/^\d{0,6}$/.test(value)) {
+                                        setPOSPin(value);
+                                        setPosPinError('');
+                                    }
+                                }}
+                                onBlur={() => {
+                                    if (!/^\d{6}$/.test(newPOSPin)) {
+                                        setPosPinError('PIN must be exactly 6 numeric digits.');
+                                    } else {
+                                        setPosPinError('');
+                                    }
+                                }}
+                                maxLength={6}
+                                inputMode="numeric"
+                                className="w-full p-2 border rounded-lg mb-1"
+                            />
+                            {posPinError && (
+                                <p className="text-red-600 text-sm mb-2">{posPinError}</p>
+                            )}
+                        </div>
+
+                        <div className="flex justify-end gap-2">
+                            <button
+                                className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+                                onClick={() => {
+                                    setShowPOSAccountModal(false);
+                                    clearInputs();
+                                }}
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                className="px-4 py-2 bg-green-700 text-white rounded-lg hover:bg-green-600  disabled:bg-green-300 disabled:cursor-not-allowed disabled:hover:bg-green-300"
+                                onClick={handlePOSAccount}
+                                disabled={isLoading || !!posPinError}
+                            >
+                                {isLoading ? "Changing..." : "Change POS PIN"}
+                            </button>
+
                         </div>
                     </div>
                 </div>
